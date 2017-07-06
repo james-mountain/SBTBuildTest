d96389dd964153a67695ef4debd6b1b7a85e73b3
James Mountain <james.w.mountain@gmail.com>
2017-07-06 14:03:29 +0000

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "SBTBuildTest"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+        
---
+++ src/main/scala/com/whiteboardcoder/hello/hw.scala
@@ -0,0 +1,10 @@
+package com.whiteboardcoder.hello
+
+/**
+  * Created by Administrator on 06/07/2017.
+  */
+object hw {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
