38e7537a8a2b277b0669d061790a3364bf7efc1f
James Mountain <james.w.mountain@gmail.com>
2017-07-06 15:15:09 +0000

    Now it makes a triangle.

--- src/main/scala/com/whiteboardcoder/hello/hw.scala
+++ src/main/scala/com/whiteboardcoder/hello/hw.scala
@@ -4,7 +4,13 @@ package com.whiteboardcoder.hello
   * Created by Administrator on 06/07/2017.
   */
 object hw {
+  def maketriangle() : Unit = {
+    (1 to 20).foreach {
+      i => println("X" * i)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    maketriangle()
   }
 }
